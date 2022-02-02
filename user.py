1a2f4bf8c58c9ec0148e007248784c786986ade2
hey im late <rantanilkku.lauri@gmail.com>
2022-02-02 14:10:57 +0200

    Task 1: added 4 new player types. Also added a function to the user class to get the amount of non-missed simulations for the user.

--- user.py
+++ user.py
@@ -49,6 +49,14 @@ class User(metaclass=ABCMeta):
             )
         )
 
+    def non_missed_simulations_completed(self) -> int:
+        """Return amount of non-missed simulations user has completed"""
+        count = 0
+        for i in self.history:
+            if (i.outcome != 'MISS'):
+                count += 1
+        return count
+
     @property
     def simulations_completed(self) -> int:
         """Return amount of simulations user has completed."""
@@ -62,7 +70,7 @@ class User(metaclass=ABCMeta):
         )
 
 
-# TODO(Task 1): Implement your own user classes.
+# (Task 1): Implement your own user classes.
 # All classes should be inherited from the above User class.
 # See the DummyUser class below user for an example.
 class DummyUser(User):
@@ -81,3 +89,104 @@ class DummyUser(User):
         # In your solution, tweak this logic to mimick your chosen user types instead
         # of picking a random simulation outcome
         return random.choice(SIMULATION_OUTCOMES)
+
+
+class QuickLearner(User):
+    """QuickLearner user class."""
+
+    def __init__(self) -> None:
+        """Init the object."""
+        super(QuickLearner, self).__init__(type="QuickLearner")
+
+    def _get_simulation_outcome(self) -> str:
+
+        def success_chance() -> float:
+            """
+            The percentage chance of successfully detecting a phishing email.
+            Follows the logistic function (x+1)/(x+10), where in this case x = non-missed simulations.
+            This way the initial chance of success is 0.1, after which it keeps rising yet never reaches 1
+            """
+            return (self.non_missed_simulations_completed() + 1) / (self.non_missed_simulations_completed() + 10)
+
+        # 10% chance to miss the email
+        if random.random() < 0.1:
+            return SIMULATION_OUTCOMES[1]  # MISS
+        elif random.random() < success_chance():
+            return SIMULATION_OUTCOMES[0]  # SUCCESS
+        else:
+            return SIMULATION_OUTCOMES[2]  # FAIL
+
+
+class SlowLearner(User):
+    """SlowLearner user class."""
+
+    def __init__(self) -> None:
+        """Init the object."""
+        super(SlowLearner, self).__init__(type="SlowLearner")
+
+    def _get_simulation_outcome(self) -> str:
+
+        def success_chance() -> float:
+            """
+            The percentage chance of successfully detecting a phishing email.
+            Follows the logistic function (x+3)/(x+30), where in this case x = non-missed simulations.
+            This way the initial chance of success is 0.1, after which it keeps rising yet never reaches 1
+            SlowLearners are 3 times slower than QuickLearners.
+            """
+            return (self.non_missed_simulations_completed() + 3) / (self.non_missed_simulations_completed() + 30)
+
+        # 10% chance to miss the email
+        if random.random() < 0.1:
+            return SIMULATION_OUTCOMES[1]  # MISS
+        elif random.random() < success_chance():
+            return SIMULATION_OUTCOMES[0]  # SUCCESS
+        else:
+            return SIMULATION_OUTCOMES[2]  # FAIL
+
+
+class BusyQuickLearner(User):
+    """
+    BusyQuickLearner user class.
+    BusyQuickLearners are the same as QuickLearners, but have a 50% chance to miss the email instead of 10%.
+    """
+
+    def __init__(self) -> None:
+        """Init the object."""
+        super(BusyQuickLearner, self).__init__(type="BusyQuickLearner")
+
+    def _get_simulation_outcome(self) -> str:
+
+        def success_chance() -> float:
+            return (self.non_missed_simulations_completed() + 1) / (self.non_missed_simulations_completed() + 10)
+
+        # 50% chance to miss the email
+        if random.random() < 0.5:
+            return SIMULATION_OUTCOMES[1]  # MISS
+        elif random.random() < success_chance():
+            return SIMULATION_OUTCOMES[0]  # SUCCESS
+        else:
+            return SIMULATION_OUTCOMES[2]  # FAIL
+
+
+class BusySlowLearner(User):
+    """
+    BusySlowLearner user class.
+    BusySlowLearners are the same as SlowLearners, but have a 50% chance to miss the email instead of 10%.
+    """
+
+    def __init__(self) -> None:
+        """Init the object."""
+        super(BusySlowLearner, self).__init__(type="BusySlowLearner")
+
+    def _get_simulation_outcome(self) -> str:
+
+        def success_chance() -> float:
+            return (self.non_missed_simulations_completed() + 3) / (self.non_missed_simulations_completed() + 30)
+
+        # 50% chance to miss the email
+        if random.random() < 0.5:
+            return SIMULATION_OUTCOMES[1]  # MISS
+        elif random.random() < success_chance():
+            return SIMULATION_OUTCOMES[0]  # SUCCESS
+        else:
+            return SIMULATION_OUTCOMES[2]  # FAIL
